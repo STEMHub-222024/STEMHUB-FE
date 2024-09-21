49d688f37f6d7d82709f743d638ded75e3509069
Duc Nguyen <duc.nguyen@vj-partner.com>
2024-09-22 00:03:24 +0700

    fix loading images banner

--- src/components/Common/SlideShow/SlideItem.js
+++ src/components/Common/SlideShow/SlideItem.js
@@ -1,21 +1,29 @@
 import classNames from 'classnames/bind';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import Image from '~/components/Common/Image';
 import styles from './SlideShow.module.scss';
 
 const cx = classNames.bind(styles);
 
-const SlideItem = memo(({ barber, index }) => (
-    <div className={cx('wrapper', { [`slide-item-${index + 1}`]: true })} key={barber.bannerId}>
-        <div className={cx('left')}>
-            <h2 className={cx('heading')}>{barber.title}</h2>
-            <p className={cx('desc')}>{barber.content}</p>
-        </div>
-        <div className={cx('right')}>
-            <Image src={barber.image} alt={barber.title} />
-        </div>
-    </div>
-));
+const SlideItem = memo(({ barber, index }) => {
+    const slideClass = useMemo(() => cx('wrapper', `slide-item-${index + 1}`), [index]);
 
+    return (
+        <div className={slideClass}>
+            <div className={cx('left')}>
+                <h2 className={cx('heading')}>{barber.title}</h2>
+                <p className={cx('desc')}>{barber.content}</p>
+            </div>
+            <div className={cx('right')}>
+                <Image
+                    src={barber.image}
+                    alt={barber.title}
+                    className={cx('image')}
+                    loading="lazy"
+                />
+            </div>
+        </div>
+    );
+});
 
-export default SlideItem
+export default SlideItem;
--- src/components/Common/SlideShow/SlideShow.js
+++ src/components/Common/SlideShow/SlideShow.js
@@ -1,56 +1,60 @@
-import { useEffect, useMemo } from 'react';
+import { lazy, Suspense, useEffect, useMemo, useState } from 'react';
 import Slider from 'react-slick';
 import classNames from 'classnames/bind';
-import SlideItem from './SlideItem';
 import SlideNextArrow from '~/components/Common/SlideNextArrow';
 import SlidePrevArrow from '~/components/Common/SlidePrevArrow';
-
-// redux
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getBannerAsync } from '~/app/slices/slideShowSlice';
-import { selectBanner } from '~/app/selectors';
-
 import styles from './SlideShow.module.scss';
 
+const SlideItem = lazy(() => import('./SlideItem'));
 const cx = classNames.bind(styles);
+
 function SlideShow() {
     const dispatch = useDispatch();
-    const { data } = useSelector(selectBanner);
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        const fetchApi = async () => {
-            try {
-                await dispatch(getBannerAsync()).unwrap();
-            } catch (rejectedValueOrSerializedError) {}
-        };
-        fetchApi();
-    }, [dispatch]);
+        if (!data.length) {  
+            const fetchApi = async () => {
+                try {
+                    const response = await dispatch(getBannerAsync()).unwrap();
+                    if (response) {
+                        setData(response);
+                    }
+                } catch (error) {
+                    console.error('Error fetching banners:', error);
+                }
+            };
+            fetchApi();
+        }
+    }, [data, dispatch]);
 
-    const sliderSettings = useMemo(
-        () => ({
-            dots: true,
-            infinite: true,
-            className: cx('center'),
-            speed: 900,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 3000,
-            cssEase: 'linear',
-            adaptiveHeight: true,
-            waitForAnimate: false,
-            pauseOnHover: true,
-            nextArrow: <SlideNextArrow />,
-            prevArrow: <SlidePrevArrow />,
-        }),
-        [],
-    );
+    const sliderSettings = useMemo(() => ({
+        dots: true,
+        infinite: true,
+        className: cx('center'),
+        speed: 900,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        cssEase: 'linear',
+        adaptiveHeight: true,
+        waitForAnimate: false,
+        pauseOnHover: true,
+        nextArrow: <SlideNextArrow />,
+        prevArrow: <SlidePrevArrow />,
+        lazyLoad: 'ondemand',
+    }), [cx]);
 
     return (
         <div className={cx('slider-container')}>
             <Slider {...sliderSettings}>
                 {data?.map((barber, index) => (
-                    <SlideItem barber={barber} index={index} key={barber.bannerId} />
+                    <Suspense fallback={<div>Loading item...</div>} key={barber.bannerId}>
+                        <SlideItem barber={barber} index={index} />
+                    </Suspense>
                 ))}
             </Slider>
         </div>
