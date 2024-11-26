17d1da3d5c9b4be6b2db75fcca788760333aa2fb
dattv23 <datvan635@gmail.com>
2024-11-26 20:34:38 +0700

    chore: remove code no use

--- src/pages/Admin/Dashboard/TopicChart.js
+++ src/pages/Admin/Dashboard/TopicChart.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from 'react';
+import { memo } from 'react';
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
@@ -6,18 +6,6 @@ ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
 const TopicChart = memo(({ data }) => {
     const colors = ['#ff6384', '#36a2eb', '#ffce56', '#82ca9d', '#ff9f40', '#4bc0c0', '#9966ff', '#c9cbcf'];
-
-    const [legendPosition, setLegendPosition] = useState('right');
-
-    useEffect(() => {
-        const updateLegendPosition = () => {
-            setLegendPosition(window.innerWidth <= 768 ? 'top' : 'right');
-        };
-
-        updateLegendPosition();
-        window.addEventListener('resize', updateLegendPosition);
-        return () => window.removeEventListener('resize', updateLegendPosition);
-    }, []);
 
     const chartData = {
         labels: data.map((item) => item.name),
