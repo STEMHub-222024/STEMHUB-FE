3e9c8f1fb3c013080ef6c2d8516c484ad74aa2fc
Duc Nguyen <duc.nguyen@vj-partner.com>
2024-12-29 19:44:11 +0700

    comment logic not use

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,4 +1,4 @@
-import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from '@google/generative-ai';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 import { toast } from 'react-toastify';
 
 const apiKey = process.env.REACT_APP_STEAM_GEMINI_AI_KEY;
@@ -8,32 +8,32 @@ const model = genAI.getGenerativeModel({
     model: 'gemini-1.5-flash',
 });
 
-const generationConfig = {
-    temperature: 0.7,
-    topP: 0.9,
-    topK: 50,
-    maxOutputTokens: 1000,
-    responseMimeType: 'text/plain',
-};
+// const generationConfig = {
+//     temperature: 0.7,
+//     topP: 0.9,
+//     topK: 50,
+//     maxOutputTokens: 1000,
+//     responseMimeType: 'text/plain',
+// };
 
-const safetySettings = [
-    {
-        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-];
+// const safetySettings = [
+//     {
+//         category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+//         threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+//     },
+//     {
+//         category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+//         threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+//     },
+//     {
+//         category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+//         threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+//     },
+//     {
+//         category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+//         threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+//     },
+// ];
 
 async function run(prompt, chatHistory = []) {
     try {
